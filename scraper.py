03bc4011baa6c2559b1ff53f54cd12deaec03bf9
barbaramaseda <barbara.maseda@gmail.com>
2016-03-07 01:52:14 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,23 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+import scraperwiki
+import lxml.html
+#
+# # Read in a page
+url = 'http://careerssearch.bbc.co.uk/jobs/search'
+html = scraperwiki.scrape(url)
+#create an empty dictionary variable called record - this will be added to in a line later on (look for record ['job'])
+record = {}
+root = lxml.html.fromstring(html)
+# # Find something on the page using css selectors
+jobtitles = root.cssselect("ul li a h3 span.job-list-title")
+for job in jobtitles:
+  jobhtml = lxml.html.tostring(job)
+  jobhtmlpart1 = jobhtml.split('>')[1]
+  jobhtmlpart2 = jobhtmlpart1.split('<')[0]
+  print 'job title html:', jobhtml
+  print 'job title cleaned up:', jobhtmlpart2
+  record['job'] = jobhtmlpart2
+  print record
+  scraperwiki.sqlite.save(unique_keys=['job'], data=record)
